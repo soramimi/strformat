b02538fdd04736914c6784e2048a2c2036d44a7f
soramimi <fi7s-fct@asahi-net.or.jp>
2026-08-08 02:49:51 +0900

    Fix unsigned negation warnings and remove unused variable in strformat.h

--- include/strformat.h
+++ include/strformat.h
@@ -95,7 +95,6 @@ public:
 	{
 		Header *h = (Header *)default_buffer;
 		*h = {};
-		size_t n = sizeof(Header);
 		h->capacity = sizeof(default_buffer) - sizeof(Header);
 	}
 	~QuickAlloc()
@@ -586,7 +585,7 @@ private:
 			using U = std::make_unsigned_t<decltype(val)>;
 			U u = (U)val;
 			if (sign) {
-				u = -u;
+				u = 0u - u;
 			}
 			while (u != 0) {
 				int c = u % 10 + '0';
@@ -634,7 +633,9 @@ private:
 			bool sign = (val < 0);
 			using U = std::make_unsigned_t<decltype(val)>;
 			U u = (U)val;
-			if (sign) u = -u;
+			if (sign) {
+				u = 0u - u;
+			}
 
 			while (u != 0) {
 				int c = u % 10 + '0';
